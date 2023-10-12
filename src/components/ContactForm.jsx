6fa87ac3dfc16204827ef2b412e975d917717a66
Alchamyst <ashley@ashley-yetman.co.uk>
2023-10-12 02:58:06 +0100

    add ContactForm component + basic form logic.

---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,58 @@
+import '../styles/contactForm.css';
+import { useState } from 'react';
+
+export default function ContactForm() {
+
+    const [name, setName] = useState('');
+    const [phone, setphone] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+    const [error, setError] = useState('');
+    const [formSubmitted, setFormSubmitted] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        setError('');
+
+        console.log(name, phone, email, message)
+
+        if(!name) {
+            return setError('Name is empty. Please fill out your name.')
+        }
+        if(!phone && !email) {
+            return setError('Please leave your email or a phone number.')            
+        }
+        if(!message) {
+            return setError('Message is empty. please type a message.')
+        }
+        setFormSubmitted(true);
+
+    }
+
+    return(
+        <div className='contact-form'>
+            {!formSubmitted &&
+            <form>
+                <label htmlFor='name'>Full Name</label>
+                <input type='text' name='name' id='name' value={name} onChange={ (e) => setName(e.target.value)} />
+
+                <label htmlFor='phone'>Phone Number</label>
+                <input type='tel' name='phone' id='phone' value={phone} onChange={ (e) => setphone(e.target.value)}/>
+
+                <label htmlFor='email'>Email</label>
+                <input type='email' name='email' id='email' value={email} onChange={ (e) => setEmail(e.target.value)}/>
+
+                <label htmlFor='message'>Message</label>
+                <textarea name='message' id='message' value={message} onChange={ (e) => setMessage(e.target.value)}/>
+
+                {error && <p className="required">{error}</p>}
+
+                <button type='submit' onClick={handleSubmit}>Send Message</button>
+            </form>}
+            {formSubmitted && <p className='success'>Form Submitted Successfully! <br />(This is placeholder text)</p>}
+        </div>
+    );
+}
+
+
--- src/components/ContactPage.jsx
+++ src/components/ContactPage.jsx
@@ -1,10 +1,13 @@
 import '../styles/contactPage.css';
+import ContactForm from './ContactForm';
 
 export default function ContactPage() {
     return(
         <div className="main">
             <main>
-                <p>This is my ContactPage component.</p>
+                <h1>Contact Me</h1>
+                <p>Please fill out the form below or visit my <a href='https://www.linkedin.com/in/ashley-yetman/'>LinkedIn</a> profile to get in contact.</p>
+                <ContactForm />
             </main>
         </div>
     );
