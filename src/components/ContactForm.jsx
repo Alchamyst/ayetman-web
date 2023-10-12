e89219273b57c074946c954d0d8e3ac1fff48039
Alchamyst <ashley@ashley-yetman.co.uk>
2023-10-13 00:25:18 +0100

    add enhanced validation checks for name and phone fields.

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -10,8 +10,9 @@ export default function ContactForm() {
     const [email, setEmail] = useState('');
     const [message, setMessage] = useState('');
     const [honeyPot, sethoneyPot] = useState('');
+
     const [error, setError] = useState('');
-    const [formValidated, setFormValidated] = useState(false);
+
     const [formSubmitted, setFormSubmitted] = useState(false);
 
     const form = useRef();
@@ -20,39 +21,96 @@ export default function ContactForm() {
         e.preventDefault();
         setError('');
 
-        if(honeyPot) {
-            return setFormSubmitted(true);    
-        }
-        if(!name) {
-            return setError('Name is empty. Please fill out your name.')
-        }
-        if(!phone && !email) {
-            return setError('Please leave your email or a phone number.')            
-        }
-        if(!message) {
-            return setError('Message is empty. Please type a message.')
-        }
+        if(honeyPot) return setFormSubmitted(true);  
 
-        // do validation checks
-        // setFormValidated(true);
-        setError('Sorry, form submissions are disabled right now.');
+        const formValidated = checkFormValidation(name, phone, email, message);
 
         if (formValidated) {
-            emailjs.sendForm(import.meta.env.VITE_EMAILJS_SERVICE_ID, import.meta.env.VITE_EMAILJS_TEMPLATE_ID, form.current, import.meta.env.VITE_EMAILJS_PUBLIC_KEY )
-            .then((result) => {
-                console.log(result.text);
-                setFormSubmitted(true);
-            }, (error) => {
-                setError(error);
-            });   
+            setFormSubmitted(true);
+            console.log(`Updated! formSubmitted=${formSubmitted}`);
+
+            // emailjs.sendForm(import.meta.env.VITE_EMAILJS_SERVICE_ID, import.meta.env.VITE_EMAILJS_TEMPLATE_ID, form.current, import.meta.env.VITE_EMAILJS_PUBLIC_KEY )
+            // .then((result) => {
+            //     console.log(result.text);
+            //     setFormSubmitted(true);
+            // }, (error) => {
+            //     setError(error);
+            // });   
         }
+
+
     }
 
-    return(
+    const checkFormValidation = (name, phone, email, message) => {
+        console.log(`name=${name} phone=${phone} email=${email} message=${message}`);
+
+        if(nameValidation(name) == false) return false;
+        if(phoneValidation(phone) == false) return false;
+        if(emailValidation(email) == false) return false;
+        if(messageValidation(message) == false) return false;
+
+        return true;
+    }
+
+    const nameValidation = (name) => {
+        if( !name ) {
+            setError('Name is empty. Please fill out your name.');
+            return false;
+        } 
+
+        if( String(name).length > 50 ) {
+            setError('Name too long. Please shorten to 50 chars or less.');
+            return false;
+        }
+
+        return true;  
+    } 
+
+    const phoneValidation = (phone) => {
+        const phoneRegex  = new RegExp("^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$");
+
+        if(!phone) return true;   
+
+        if(!phoneRegex.test(phone)) {
+            setError('Please enter a valid phone number, with no spaces.');
+            return false; 
+        } 
+
+        return true;      
+    }
+
+    const emailValidation = (email) => {
+        // const emailRegex  = new RegExp("^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$");
+
+        const emailRegex  = new RegExp("^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$");
+
+        if(!email) {
+            setError('Please enter your email.');  
+            return false;  
+        }
+
+        // if(!emailRegex.test(email)) {
+        //     setError('Please enter a valid email.');
+        //     return false; 
+        // } 
+        
+        return true; 
+    }
+
+    const messageValidation = (message) => {
+        if(!message) {
+            setError('Message is empty. Please type a message.');
+            return false
+        }
+
+        return true; 
+    }
+
+    return (
         <div className='contact-form'>
             {!formSubmitted &&
             <form ref={form}>
-                <label htmlFor='name'>Full Name</label>
+                <label htmlFor='name'>Your Name</label>
                 <input type='text' name='name' id='name' value={name} onChange={ (e) => setName(e.target.value)} />
 
                 <label htmlFor='phone'>Phone Number</label>
