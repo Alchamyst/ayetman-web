e346ac406a3b4294aec0d1bd37249371cc95fcaa
Alchamyst <ashley@ashley-yetman.co.uk>
2023-10-13 16:40:10 +0100

    update aboutMePage content.

--- src/views/AboutMePage.jsx
+++ src/views/AboutMePage.jsx
@@ -1,24 +1,26 @@
-import SocialLinks from '../components/SocialLinks';
 import '../styles/aboutMePage.css';
+import SocialLinks from '../components/SocialLinks';
+import ashleyImg from '../assets/ashley.jpg';
 
 export default function AboutMePage() {
     return(
         <div className='main'>
-            <main className='about-me'>
+            <main>
                 <div className='about-me'>
                     <h1>Hey, I'm Ashley.</h1>
+                    <img className='ash-img' src={ashleyImg} />
 
                     <p>
-                        Hello, I'm Ashley, a driven web developer and digital strategist with a strong foundation in the dynamic landscape of IT and online marketing.  
+                        A driven web developer and digital strategist with a strong foundation in the dynamic landscape of IT and online marketing.  
                         My journey in the world of technology and marketing has equipped me with a unique blend of skills and insights.
                     </p>
                     <p>
                         When it comes to coding, my enthusiasm knows no bounds. JavaScript is my playground, and I thrive in the worlds of Node.js and React. 
                         These skills have empowered me to collaborate effectively with clients' development teams, crafting optimized user experiences that leave a lasting impression.
                     </p>
-                </div>
-                <SocialLinks />
+                </div> 
             </main>
+            <SocialLinks />
         </div>
     );
 }
