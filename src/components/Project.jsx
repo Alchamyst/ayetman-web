b35e5c144531646cfa2415b33ff475e53105d623
Alchamyst <ashley@ashley-yetman.co.uk>
2023-10-14 01:29:22 +0100

    add props and styling to Project component.

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,9 +1,47 @@
 import '../styles/project.css';
+import ListBlock from '../components/ListBlock';
 
 export default function Project(props) {
     return(
         <>
-            <p>This is my Project component.</p>
+            <div className='project-card'>
+
+                <div className='project-card-header'>
+                    <h2 className='project-title'>{props.name}</h2>
+                    <div className='project-links'>
+                        <a href={props.github}>View on GitHub</a> 
+                        {props.liveUrl && <a href={props.liveUrl}>Visit Live Project</a>}
+                    </div>
+                </div>
+
+                <div className='project-image-container'>
+                    <img src={props.image} />
+                </div>
+
+                <div className='project-info'>
+                    <ListBlock list={props.skills} />
+                    <p>{props.description}</p>
+                </div>
+            </div>
         </>
     );
 }
+
+// name={expensifyProject.name} 
+// skills={expensifyProject.skills} 
+// image={expensifyProject.image} 
+// github={expensifyProject.github}
+// liveUrl={expensifyProject.liveUrl}
+// description={expensifyProject.description}
+
+{/* <h2>Expensify App</h2>
+    <div style={{display:'flex'}}>
+        <div style={{'max-width':'20em'}}>
+            <img src={projectIcon} width="200em" alt="app picture" height="200em" border="1px"></img>
+            <ListBlock list={['Firebase','Javascript','Node.js','React']} />
+        </div>
+        <div>
+            <a href='#'>View on GitHub</a> // <a href='#'>Visit Live Project</a>
+            <p>Here is a description of my app. This would talk about the experience and technology used in the app.</p>
+        </div>
+    </div> */}
--- src/components/ProjectsList.jsx
+++ src/components/ProjectsList.jsx
@@ -1,9 +1,29 @@
 import '../styles/projectsList.css';
+import Project from './Project';
+// import ListBlock from '../components/ListBlock';
+import projectIcon from '../assets/link-icon.svg';
 
 export default function ProjectsList(props) {
+
+    const expensifyProject = {
+        name: "Expensify App",
+        skills: ['Firebase','Javascript','Node.js','React'],
+        image: projectIcon,
+        github: "https://github.com/Alchamyst/expensify-app",
+        liveUrl: '#',
+        description: 'Here is a description of my app. This would talk about the experience and technology used in the app.'
+    }
+
     return(
         <>
-            <p>This is my ProjectsList component.</p>
+            <Project 
+                name={expensifyProject.name} 
+                skills={expensifyProject.skills} 
+                image={expensifyProject.image} 
+                github={expensifyProject.github}
+                liveUrl={expensifyProject.liveUrl}
+                description={expensifyProject.description}
+            />
         </>
     );
 }
