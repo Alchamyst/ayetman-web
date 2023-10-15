e663a0d92401e372be414f281fe2af606f72ef31
Alchamyst <ashley@ashley-yetman.co.uk>
2023-10-15 01:06:26 +0100

    enable ProjectsPage.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar(props) {
             <ul>
                 <li><a href="/">&lt;about-me&gt;</a></li>
                 <li><a href="/experience">&lt;experience&gt;</a></li>
-                {/* <li><a href="/projects">&lt;projects&gt;</a></li> */}
+                <li><a href="/projects">&lt;projects&gt;</a></li>
                 <li><a href="/contact">&lt;contact&gt;</a></li>
             </ul>
             {props.children}
