4aeb53563c0a8d91108c94e4d35abdb633760cda
Alchamyst <ashley@ashley-yetman.co.uk>
2023-10-17 23:23:15 +0100

    add styling to historyListItem component + add content expansion toggle state.

--- src/components/HistoryListItem.jsx
+++ src/components/HistoryListItem.jsx
@@ -1,9 +1,25 @@
+import { useState } from 'react';
 import '../styles/historyListItem.css';
 
 export default function HistoryListItem(props) {
+
+    const [ isExpanded, setExpanded ] = useState(false);
+
+    const handleContentExpansion = () => {
+        if(isExpanded == true) return setExpanded(false);
+
+        if(isExpanded == false){
+            setExpanded(true);
+        }
+    }
+
     return (
         <div className='history-list-item'>
-            <div className='history-compact'>
+            <div className={`history-compact ${isExpanded ? "history-compact-expanded": ""}`} onClick={handleContentExpansion}>
+                <div className={`expander${isExpanded ? "-open" : ""}`}>
+                    {!isExpanded && <p>&#x25B6;</p>}
+                    {isExpanded && <p>&#x25BC;</p>}
+                </div>
                 <div className='history-titles'>
                     <p className='history-designation'>{props.designation}</p>
                     <p className='history-organisation'>{props.organisation}</p>
@@ -13,11 +29,11 @@ export default function HistoryListItem(props) {
                     <p className='history-end-date'>{props.endYearMonth}</p>
                 </div>
             </div>
-            <div className='history-expanded'>
+            {isExpanded && <div className='history-expanded'>
                 <div className='history-description'>
                     <p>{props.description}</p>
                 </div>
-            </div>
+            </div>}
         </div>
     );
 }
