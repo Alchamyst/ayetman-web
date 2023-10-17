44fba9297ab12187d13e56c0a8f0a36a16e1c085
Alchamyst <ashley@ashley-yetman.co.uk>
2023-10-17 21:54:47 +0100

    add basic div layout to HistoryListItem component.

--- src/components/HistoryListItem.jsx
+++ src/components/HistoryListItem.jsx
@@ -3,14 +3,21 @@ import '../styles/historyListItem.css';
 export default function HistoryListItem(props) {
     return (
         <div className='history-list-item'>
-            <p>This is my HistoryListItem component.</p>
-            <ul>
-                <li>organisation: {props.organisation}</li>  
-                <li>designation: {props.designation}</li>
-                <li>startYearMonth: {props.startYearMonth}</li>
-                <li>endYearMonth: {props.endYearMonth}</li>
-                <li>description: {props.description}</li>               
-            </ul>
+            <div className='history-compact'>
+                <div className='history-titles'>
+                    <p className='history-designation'>{props.designation}</p>
+                    <p className='history-organisation'>{props.organisation}</p>
+                </div>
+                <div className='history-dates'>
+                    <p className='history-start-date'>{props.startYearMonth}</p>
+                    <p className='history-end-date'>{props.endYearMonth}</p>
+                </div>
+            </div>
+            <div className='history-expanded'>
+                <div className='history-description'>
+                    <p>{props.description}</p>
+                </div>
+            </div>
         </div>
     );
 }
