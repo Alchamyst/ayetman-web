09a9e10c53a434fd08e6399974439851ef12c1c1
Alchamyst <ashley@ashley-yetman.co.uk>
2023-10-19 14:56:26 +0100

    update ProjectPage styles.

--- src/views/ProjectsPage.jsx
+++ src/views/ProjectsPage.jsx
@@ -7,8 +7,8 @@ export default function ProjectsPage() {
             <main>
                 <div className='content-box'>
                     <h1 className='projects-page-title'>My Projects</h1>
-                    <p>Below are personal projects I have worked on, along with their source code.</p>
-                    <p>There is a button to view the source code for each project on github, and some projects have a button for viewing the live version where available.</p>
+                    <p className='projects-page-description'>Below are personal projects I have worked on, along with their source code.</p>
+                    <p className='projects-page-description'>There is a button to view the source code for each project on github, and some projects have a button for viewing the live version where available.</p>
                 </div>
                 <ProjectsList />
             </main>
